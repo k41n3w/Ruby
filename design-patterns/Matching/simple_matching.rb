e454ccc50663649f5fb8ac6afd91aeeaff4171b8
Caio Ramos <kaineo@hotmail.com>
2021-08-21 16:52:53 -0300

    Pattern simple matching

--- design-patterns/Matching/simple_matching.rb
+++ design-patterns/Matching/simple_matching.rb
@@ -14,3 +14,25 @@ in ['th', orig_text, 'ja', trans_text]
   # this will get executed
   puts "Japanese translation: #{orig_text} => #{trans_text}"
 end
+
+
+p '------------------------------------------------------------------------------'
+# Exemplo:
+
+from = 'caconde'
+to   = 'são paulo'
+
+# Não sei a distancia
+# from = 'caconde'
+# to   = ''
+
+case [from, to]
+in 'caconde', 'são paulo'
+  puts 'Longa viagem!'
+in 'são josé', 'são paulo'
+  puts 'Pertinho!'
+in 'caconde', _
+  puts 'Não sei a distância :/'
+else
+  puts 'Caminho não encontrado'
+end
